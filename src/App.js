a0386ca22a898407f5543e797cc8cd407bf2eee5
Paulo de Tarso <paulo.gregorio@dataprev.gov.br>
2021-03-30 01:47:45 -0300

    Alterando pelo branch feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Saravá, meu rei!
+          Agora foi alterado no branch feature...
         </p>
         <a
           className="App-link"
